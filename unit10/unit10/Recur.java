6693eeb43dc5cd822899433f9f5a74ab3ea41522
clara j <124330368+pinkbagel@users.noreply.github.com>
2023-03-24 21:43:09 +0000

    Bell Ringer Correction

--- unit10/unit10/Recur.java
+++ unit10/unit10/Recur.java
@@ -66,19 +66,40 @@ public class Recur {
         return 4 + mystery(x - 1);
     }
 
+    //Bell Ringers!
     public static int prob1(int a, int b){
-        if (a<b)
-        return a;
-        else b + prob1(a-1, b+1);
+        if (a<b) //if a is less than b
+        return a; //stop and return a
+        else 
+        return b + prob1(a-1, b+1); //add previous b and the product of prob1 when a= a-1, b= b+1
+    }
+
+    public static int prob2(int a, int b) {
+        if (a%b ==0) //when a % b is 0
+        return b;//stop and return b
+        else
+        return prob2(b, a%b); //if not, do a % b (which is 37)
+    }
+
+    public static int prob3(int a, int b){
+        if (a==0) //when a is 0
+        return 1; // stop and return 1
+        else
+        return b*prob3(a-1,b); // returns b to the power of a because it multiplies b 
+                               //and the result of prob(a-1,b) again and again until a is 0
     }
 
     public static void main(String[] args) {
-        //function 
+        //Bell ringers
+        System.out.println(prob1(4,2));
+        System.out.println(prob2(111,74));
+        System.out.println(prob3(3,3));
+
          //Consider function fun3
          //System.out.println(fun3(5));
 
         // Consider function f
-         System.out.println(f(6));
+         //System.out.println(f(6));
 
         // Consider function has
         // System.out.println(has("hello"));
