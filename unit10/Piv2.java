920695da088d5b1422bb4a917ac80daf2ef75fbf
clara j <124330368+pinkbagel@users.noreply.github.com>
2023-03-31 21:36:47 +0000

    Revised Pi Function - Clara

--- unit10/Piv2.java
+++ unit10/Piv2.java
@@ -1,29 +1,29 @@
 package unit10;
+
 public class Piv2 {
 
-//π = 4(1−(1/3)+(1/5)−(1/7)+(1/9)−⋯)
+    // π = 4(1−(1/3)+(1/5)−(1/7)+(1/9)−⋯)
 
     public static double piDay(int n, boolean subtract, double pi, double denom) {
         if (n == 0) {
-            pi = 4*(1-1/denom);
+            pi = 4 * (1 - 1 / denom);
+        } else {
+            if (subtract) {
+                pi -= 1 / denom;
+                denom += 2.0;
+                return piDay(n - 2, !subtract, pi, denom);
+            } else if (!subtract) {
+                pi += 1 / denom;
+                denom += 2.0;
+                return piDay(n - 2, !subtract, pi, denom);
+
+            }
         }
-        else {
-                if (subtract) {
-                    pi -= 1/denom;
-                    denom += 2.0;
-                    piDay(n-2, !subtract, pi, denom);
-                }
-                else if (!subtract){
-                    pi += 1/denom;
-                    denom += 2.0;
-                    piDay(n-2, !subtract, pi, denom);
-                } 
-        }
-        return pi=4*(1-1/denom);
+        return pi = 4 * (1 - 1 / denom);
     }
 
     public static void main(String args[]) {
-        double pi = piDay(60, false, 0.0, 1.0);
+        double pi = piDay(68, false, 0.0, 1.0);
         System.out.println("Pi = " + pi);
     }
 }
