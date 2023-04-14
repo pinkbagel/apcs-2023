f76dfd7c96e83b3bc36c28db8933fa5e66ae1687
clara j <124330368+pinkbagel@users.noreply.github.com>
2023-04-14 04:29:19 +0000

    RandomStringChooser (Clara)

--- unit11/RandomStringChooser.java
+++ unit11/RandomStringChooser.java
@@ -1,5 +1,6 @@
 package unit11;
 
+import java.time.Year;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,7 +9,23 @@ import java.util.List;
 // https://secure-media.collegeboard.org/digitalServices/pdf/ap/ap16_frq_computer_science_a.pdf#page=2
 
 public class RandomStringChooser {
-    // TODO part a
+    private ArrayList<String> remain;
+
+    public RandomStringChooser(String[] val) {
+        remain = new ArrayList<String>();
+        for (String value : val) {
+            remain.add(value);
+        }
+    }
+
+    public String getNext() {
+        if (remain.size() > 0) {
+            int index = (int) Math.random() * 10;
+            return remain.remove(index);
+        } else {
+            return "NONE";
+        }
+    }
 
     static void check(boolean test) throws AssertionError {
         if (!test)
@@ -17,31 +34,31 @@ public class RandomStringChooser {
 
     public static void main(String[] args) {
         // Uncomment the following lines to test your code: select and Ctrl+/
-        // String[] wordArray = { "wheels", "on", "the", "bus" };
-        // RandomStringChooser sChooser = new RandomStringChooser(wordArray);
-        // for (int k = 0; k < 6; k++) {
-        // String next = sChooser.getNext();
-        // System.out.print(next + " ");
-        // if (k < 4)
-        // check(Arrays.stream(wordArray).anyMatch(next::equals));
-        // else
-        // check(next.equals("NONE"));
+        String[] wordArray = { "wheels", "on", "the", "bus" };
+        RandomStringChooser sChooser = new RandomStringChooser(wordArray);
+        for (int k = 0; k < 6; k++) {
+            String next = sChooser.getNext();
+            System.out.print(next + " ");
+            if (k < 4)
+                check(Arrays.stream(wordArray).anyMatch(next::equals));
+            else
+                check(next.equals("NONE"));
 
-        // }
-        // System.out.println();
+        }
+        System.out.println();
 
-        // String word = "cat";
-        // RandomLetterChooser letterChooser = new RandomLetterChooser(word);
-        // for (int k = 0; k < 4; k++) {
-        // String next = letterChooser.getNext();
-        // System.out.print(next);
-        // if (k < 3)
-        // check(word.indexOf(next) != -1);
-        // else
-        // check(next.equals("NONE"));
-        // }
-        // System.out.println();
-        // System.out.println("Happy Panda! \uD83D\uDC3C");
+        String word = "cat";
+        RandomLetterChooser letterChooser = new RandomLetterChooser(word);
+        for (int k = 0; k < 4; k++) {
+            String next = letterChooser.getNext();
+            System.out.print(next);
+            if (k < 3)
+                check(word.indexOf(next) != -1);
+            else
+                check(next.equals("NONE"));
+        }
+        System.out.println();
+        System.out.println("Happy Panda! \uD83D\uDC3C");
     }
 }
 
@@ -52,8 +69,13 @@ class RandomLetterChooser extends RandomStringChooser {
      * Precondition: str contains only letters.
      */
     public RandomLetterChooser(String str) {
-        // TODO part b
+        super(getSingleLetters(str));
+        // ArrayList<String> y = new ArrayList<String>;
+        // for(int i = 0; i<str.length(); i++){
+        // y[i]=str.indexOf(i); 
     }
+
+
 
     /**
      * Returns an array of single-letter strings.
