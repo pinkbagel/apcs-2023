c3086562b9281bedb49802a1b3c85d78e30577c6
clara j <124330368+pinkbagel@users.noreply.github.com>
2023-05-17 16:55:54 +0000

    DoubleTrouble & TechStack (Clara)

--- unit11/DoubleTrouble.java
+++ unit11/DoubleTrouble.java
@@ -1,6 +1,6 @@
 package unit11;
 
-public class DoubleTrouble{
+public class DoubleTrouble {
 
     private int player1;
     private int player2;
@@ -9,62 +9,61 @@ public class DoubleTrouble{
     private int rollb1;
     private int rollb2;
 
-    public DoubleTrouble(int player1, int player2){
+    public DoubleTrouble(int player1, int player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public DoubleTrouble(){
+    public DoubleTrouble() {
         player1 = 10;
         player2 = 10;
     }
 
-    public void evenOrOdd(){
-        if(rolla1 + rolla2 % 2==1){
+    public void evenOrOdd() {
+        if ((rolla1 + rolla2) % 2 == 1 && (rollb1 + rollb2) != 7) {
             player1 += 1;
         }
-        if(rollb1 + rollb2% 2 ==1){
+        if ((rollb1 + rollb2) % 2 == 1 && (rollb1 + rollb2) != 7) {
             player2 += 1;
         }
     }
 
-    public void sevenOrDouble(){
-        if((rolla1+rolla2)==7){
+    public void sevenOrDouble() {
+        if ((rolla1 + rolla2) == 7) {
             player1 += 3;
         }
-        if(rolla1==rolla2){
+        if (rolla1 == rolla2) {
             player1 -= 5;
         }
-        if((rollb1+rollb2)==7){
+        if ((rollb1 + rollb2) == 7) {
             player2 += 3;
         }
-        if(rollb1==rollb2){
+        if (rollb1 == rollb2) {
             player2 -= 5;
         }
     }
 
-    public void rollDice(){
-        rolla1 = (int)((Math.random()*6)+1);
-        rolla2 = (int)((Math.random()*6)+1);
-        rollb1 = (int)((Math.random()*6)+1);
-        rollb2 = (int)((Math.random()*6)+1);
+    public void rollDice() {
+        rolla1 = (int) ((Math.random() * 6) + 1);
+        rolla2 = (int) ((Math.random() * 6) + 1);
+        rollb1 = (int) ((Math.random() * 6) + 1);
+        rollb2 = (int) ((Math.random() * 6) + 1);
     }
 
-    public static void main(String []args){
+    public static void main(String[] args) {
         DoubleTrouble play = new DoubleTrouble();
-        while(play.player1 > 0 && play.player2 > 0){
-        play.rollDice();
-        play.evenOrOdd();
-        play.sevenOrDouble();
-        System.out.println("--");
-        System.out.println("Player1:" + play.player1);
-        System.out.println("Player2:" + play.player2);
+        while (play.player1 > 0 && play.player2 > 0) {
+            play.rollDice();
+            play.evenOrOdd();
+            play.sevenOrDouble();
+            System.out.println("--");
+            System.out.println("Player1:" + play.player1);
+            System.out.println("Player2:" + play.player2);
         }
         System.out.println("---");
-        if(play.player1>play.player2){
+        if (play.player1 > play.player2) {
             System.out.println("Player 1 wins!");
-        }
-        else{
+        } else {
             System.out.println("Player 2 wins!");
         }
 
--- unit12/techstack.md
+++ unit12/techstack.md
@@ -11,3 +11,19 @@ Record your discoveries in the list below, as [Markdown](https://www.markdowngui
 ## Technologies
 
 - [Clash of Clans](https://supercell.com/en/games/clashofclans/) is programmed with Objective C/C++ clients and a Java server. Source: [quora](https://www.quora.com/What-programming-language-is-used-in-Clash-of-Clan-Game?share=1)
+
+- [Minecraft] is programmed with Java. Source: [aiplusinfo](https://www.aiplusinfo.com/blog/what-language-is-minecraft-coded-in-how-can-minecraft-help-kids-code/#:~:text=Minecraft%20was%20originally%20developed%20using,to%20learn%20programming%20with%20Minecraft!)
+
+- [Schoology] is programmed mainly with DHP Source: [w3techs] (https://w3techs.com/sites/info/schoology.com#:~:text=Site%20Info%20%2D%20Schoology.com,technologies%20used%20by%20Schoology.com.&text=Drupal%20is%20an%20open%20source,originally%20developed%20by%20Dries%20Buytaert.&text=PHP%20is%20a%20scripting%20language%20for%20creating%20websites.)
+
+- [Youtube] is mostly programmed in Python, but also JavaScript, TypeScript, Java, C++, C, and Go. Source: [quora](https://www.quora.com/What-programming-is-used-in-YouTube)
+
+- [Github] is mainly programmed with Ruby and uses JavaScript for front end. Source [quora](https://www.quora.com/What-programming-language-is-GitHub-written-in)
+
+- [Google]'s top programming languages are Python, C++, Go, and Java. Source: (https://www.interviewkickstart.com/blog programming-languages-google-tech-interview#:~:text=Python%2C%20C%2B%2B%2C%20Go%2C,any%20programming%20language%20they%20like.)
+
+-[Amazon] is programmed with JavaScript(front end) and Java, C++, and Perl(Backend) Source: (https://www.interviewkickstart.com/blog/programming-languages-google-tech-interview#:~:text=Python%2C%20C%2B%2B%2C%20Go%2C,any%20programming%20language%20they%20like)
+
+-[KhanAcademy] uses JavaScript and Processing.js (library of functions). Source: [stackoverflow](https://stackoverflow.com/questions/15190606/what-language-is-khan-academy-cs-using#:~:text=Khan%20Academy%20uses%20JavaScript%2C%20with,to%20Javascript%20by%20John%20Resig.)
+
+-[Skyward] uses Javascript, LightBox, and Bootstrap. Source: [6sense] (https://6sense.com/company/skyward-automotive-products/5d9e01bca7537b35bb5d9f0c#:~:text=The%20top%20technologies%20used%20by,are%20JavaScript%20%2C%20LightBox%20and%20Bootstrap%20.)
